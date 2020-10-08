7979285f06ddbdbd167a095e053197916ed1663f
Diego <Diegokoga1@gmail.com>
2020-10-08 00:36:26 -0300

    Filinha do papai

---
+++ src/view/TestaFila.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.FilaEstatica;
+
+public class TestaFila {
+
+	public static void main(String[] args) {
+		FilaEstatica fila = new FilaEstatica();
+//		fila.insere(1);
+//		fila.insere(2);//testando a parada
+//		fila.insere(3);
+//		fila.insere(4);
+//		fila.insere(5);
+//		fila.insere(6);
+//		fila.insere(7);
+		while (!fila.isEmpty()) {//enquanto ela não for vazia
+			Object x = fila.retira();
+			System.out.println("Elemento " + x + " retirado");
+		}
+	}
+}
